881019c9cf06cb70db62a29261db7bb4ac7d59e6
JuanBurgos <JuanBurgos@outlook.com>
2021-06-01 21:45:30 -0400

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '914251045810798',
+        clientSecret: '67b16a405ebbd2d420d0cd5f1e955377'
+    }
 }
